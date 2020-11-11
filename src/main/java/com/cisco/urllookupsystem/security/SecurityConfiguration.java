6c5d6cba9a0867bff65f939dcbd3c151f1f4bbdc
saurabhrk8 <kulkarni.saurabh2@gmail.com>
2020-11-11 19:22:11 +0000

    bdd added

--- src/main/java/com/cisco/urllookupsystem/security/SecurityConfiguration.java
+++ src/main/java/com/cisco/urllookupsystem/security/SecurityConfiguration.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -18,8 +20,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/urlinfo").permitAll()
-                .antMatchers("/", "/*hostname_and_port*/**").access("hasRole('USER')").and()
+        http.authorizeRequests().antMatchers("/urlinfo").permitAll().antMatchers( "/h2/**").permitAll()
+                .antMatchers("/", "/*hostname_and_port*/**").access("hasRole('USER')").antMatchers("/", "/h2/**").access("hasRole('ROLE_USER')").and()
                 .formLogin();
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2/**");
     }
 }
